5ea04cf7a0ebf5a43a23201398b135c713bdf9ab
Levi Lovelock <levilove@amazon.com>
2021-05-18 21:02:27 -0700

    test

--- tamp.js
+++ tamp.js
@@ -1,19 +1,11 @@
 // ==UserScript==
-// @name         Levis tamp
-// @downloadURL	 https://raw.githubusercontent.com/levpaul/tampertest/main/tamp.js
-// @updateURL    https://raw.githubusercontent.com/levpaul/tampertest/main/tamp.js
-// @namespace	 http://tampermonkey.net/
-// @version      0.0.1
-// @description  My test thing
-// @author       levpaul
-// @match        https://levpaul.com/*
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_deleteValue
-// @grant        GM_xmlhttpRequest
-// @grant        GM_registerMenuCommand
-// @grant        GM_log
-// @grant        GM_setClipboard
+// @name Open CodeProject Links
+// @namespace http://hibbard.eu/
+// @version 0.1
+// @description  Opens links from the CodeProject newsletter
+// @match http://www.codeproject.com/script/Mailouts/*
+// @copyright 2012+, hibbard.eu
+// @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 console.log("hello world");
