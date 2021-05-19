5b1eac892451ef5fb5c3c9111ea79a2071ac8aec
Levi Lovelock <levilove@amazon.com>
2021-05-18 21:04:40 -0700

    test

--- tamp.js
+++ tamp.js
@@ -1,12 +1,14 @@
 // ==UserScript==
-// @name Open CodeProject Links
-// @namespace http://hibbard.eu/
-// @version 0.1
-// @description  Opens links from the CodeProject newsletter
-// @match http://www.codeproject.com/script/Mailouts/*
-// @copyright 2012+, hibbard.eu
-// @require http://code.jquery.com/jquery-latest.js
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://atomicobject.com/*
+// @grant        none
 // ==/UserScript==
 
-console.log("hello world");
-
+(function() {
+    'use strict';
+    console.log("hello, world!");
+})();
