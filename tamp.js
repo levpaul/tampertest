e3c3815aa623933e02db84795c2aaef6b08b9746
Levi Lovelock <levilove@amazon.com>
2021-05-18 21:00:05 -0700

    asdf

---
+++ tamp.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Levis tamp
+// @downloadURL	 https://raw.githubusercontent.com/levpaul/tampertest/main/tamp.js
+// @updateURL    https://raw.githubusercontent.com/levpaul/tampertest/main/tamp.js
+// @namespace	 http://tampermonkey.net/
+// @version      0.0.1
+// @description  My test thing
+// @author       levpaul
+// @match        https://levpaul.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @grant        GM_log
+// @grant        GM_setClipboard
+// ==/UserScript==
+
+console.log("hello world");
+
